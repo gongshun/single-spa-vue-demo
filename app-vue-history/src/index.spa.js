b04a471940abae9c30c1a2049846ee32c3bee65e
龚顺 <gongshun@gridsum.com>
2020-03-04 14:52:50 +0800

    增加single-spa自带的数据传递和解决css污染的示例

--- app-vue-history/src/index.spa.js
+++ app-vue-history/src/index.spa.js
@@ -15,6 +15,24 @@ const vueLifecycles = singleSpaVue({
   appOptions
 });
 
-const { bootstrap, mount, unmount } = vueLifecycles;
+const { bootstrap, mount: mount1, unmount: unmount1 } = vueLifecycles;
 
+//生命周期函数必须返回一个promise，而async函数总是会返回一个promise
+async function mount2(props){
+  //注册子系统的时候传递的数据
+  console.log(props);
+  //文档写的是props.customProps.authToken，实际上发现是props.authToken
+  console.log('props.authToken',props.authToken);
+
+  //给body加class,以解决全局样式污染
+  document.body.classList.add('app-vue-history')
+}
+
+async function unmount2(props){
+  //去掉body的class
+  document.body.classList.remove('app-vue-history')
+}
+
+const mount = [mount1,mount2];
+const unmount = [unmount1,unmount2];
 export { bootstrap, mount, unmount };
--- app-vue-history/src/main.js
+++ app-vue-history/src/main.js
@@ -2,6 +2,8 @@ import store from "./store";
 import Vue from 'vue';
 import App from './App.vue';
 
+import './assets/common.css';
+
 const appOptions = {
   render: (h) => h(App),
   store,
