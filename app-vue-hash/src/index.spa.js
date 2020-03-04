554ab2e0f9e36a1906672621787e433a8ff9b0a3
龚顺 <gongshun@gridsum.com>
2020-03-04 16:28:11 +0800

    增加示例：切换系统修改系统的icon

--- app-vue-hash/src/index.spa.js
+++ app-vue-hash/src/index.spa.js
@@ -11,9 +11,23 @@ const vueLifecycles = singleSpaVue({
   appOptions
 });
 
-// export const bootstrap = vueLifecycles.bootstrap;
-// export const mount = vueLifecycles.mount;
-// export const unmount = vueLifecycles.unmount;
-const { bootstrap, mount, unmount } = vueLifecycles;
 
+const { bootstrap, mount: mount1, unmount: unmount1 } = vueLifecycles;
+
+const headEle = document.querySelector('head');
+let linkEle = null ;
+// 因为新插入的icon会覆盖旧的，所以旧的不用删除，如果需要删除，可以在unmount时再插入进来
+async function mount2(props){
+  linkEle = document.createElement("link");
+  linkEle.setAttribute('rel','icon');
+  linkEle.setAttribute('href','https://gold-cdn.xitu.io/favicons/favicon.ico');
+  headEle.appendChild(linkEle);
+}
+async function unmount2(props){
+  headEle.removeChild(linkEle);
+  linkEle = null;
+}
+
+const mount = [mount1,mount2];
+const unmount = [unmount1,unmount2];
 export { bootstrap, mount, unmount };
